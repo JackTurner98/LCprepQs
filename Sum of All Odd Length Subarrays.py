5d69ac13e69c60577d27df28326774b001076ea0
Jack Turner <jackturner@cmail.carleton.ca>
2020-09-27 19:45:47 -0400

    Readded the assert

--- Sum of All Odd Length Subarrays.py
+++ Sum of All Odd Length Subarrays.py
@@ -13,4 +13,4 @@ class Solution(object):
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.sumOddLengthSubarrays([1, 2]))
+    assert solution.sumOddLengthSubarrays([1, 2]) == 3
